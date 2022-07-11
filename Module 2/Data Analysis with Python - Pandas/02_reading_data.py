da0c429ed238b3998bbe87d649a0b2e496a68bfc
Eda AYDIN <edaaydin.ea@outlook.com>
2022-07-12 00:03:35 +0300

    Create 02_reading_data.py

---
+++ Module 2/Data Analysis with Python - Pandas/02_reading_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("datasets/advertising.csv")
+df.head()
+
+"""
+Output:
+      TV  radio  newspaper  sales
+1  230.1   37.8       69.2   22.1
+2   44.5   39.3       45.1   10.4
+3   17.2   45.9       69.3    9.3
+4  151.5   41.3       58.5   18.5
+5  180.8   10.8       58.4   12.9
+"""
